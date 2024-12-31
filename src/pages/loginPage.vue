268d15deca72d76c76c5c1ef1d0a1668dcaa0e37
nnet <137188915+nnnnett@users.noreply.github.com>
2024-12-31 19:21:41 +0800

    login and registration form(done with mobile view and validation

--- src/pages/loginPage.vue
+++ src/pages/loginPage.vue
@@ -1,14 +1,116 @@
 <template>
   <q-page>
-    <div class="flex flex-center">
-      <q-form @submit.prevent="createStudent">
-        <q-input type="text" label="username" v-model="username" />
-        <q-input type="password" label="password" v-model="password" />
-        <q-btn type="submit" label="submit" :loading="loading" />
-      </q-form>
+    <!-- Main Container -->
+    <div class="main-container q-pb-lg">
+      <q-card-section>
+        <div class="q-py-lg">
+          <q-img
+            src="https://res.cloudinary.com/dqaw6ndtn/image/upload/v1734702951/assets/fjqfqywlp4zvtxv5rkau.png"
+            style="width: 90px; justify-self: center; display: flex"
+          />
+        </div>
+      </q-card-section>
+      <!-- login form -->
+      <q-card class="login-container q-pa-xl">
+        <q-form @submit.prevent="createStudent">
+          <q-card-section>
+            <div
+              class="text-h4"
+              style="color: #4b4b4b; font-weight: 500; text-align: center"
+            >
+              Sign in
+            </div>
+          </q-card-section>
+          <q-card-section class="q-pl-none q-pb-xs">
+            <div style="color: #6c7275">Username</div>
+          </q-card-section>
+          <!-- username -->
+          <div class="login-input">
+            <q-input type="text" borderless v-model="username" />
+          </div>
+          <q-card-section class="q-pl-none q-pb-xs">
+            <div style="color: #6c7275">Password</div>
+          </q-card-section>
+          <!-- password -->
+          <div class="login-input">
+            <q-input type="password" borderless v-model="password" />
+          </div>
+          <div style="width: 100%; border-radius: 14px" class="q-mt-md">
+            <!-- submit button -->
+            <q-btn
+              type="submit"
+              flat
+              label="Sign in"
+              :loading="loading"
+              style="
+                width: 100%;
+                background-color: #46af4b;
+                color: #ffffff;
+                text-transform: capitalize;
+                border-radius: 14px;
+                padding: 14px 0px;
+              "
+            />
+          </div>
+        </q-form>
+      </q-card>
+      <q-card-section class="q-my-md newCommunityTxt">
+        <div style="border: 1px solid #d9d9d9; width: 33%; height: 0px"></div>
+        <div style="width: 30%; color: #6c7275">New to our community</div>
+        <div style="border: 1px solid #d9d9d9; width: 33%; height: 0px"></div>
+      </q-card-section>
+      <q-card-section class="createAccBtn">
+        <div
+          style="width: 100%; border-radius: 14px; border: 1px solid #4b4b4b"
+        >
+          <!-- submit button -->
+          <q-btn
+            to="registerPage"
+            flat
+            label="Create An Account"
+            style="
+              width: 100%;
+              color: #4b4b4b;
+              text-transform: capitalize;
+              border-radius: 14px;
+              padding: 14px 0px;
+            "
+          />
+        </div>
+      </q-card-section>
     </div>
   </q-page>
 </template>
+
+<style lang="sass" scoped>
+.main-container
+  height: auto
+  width: 100vw
+.login-container
+  height: auto
+  width: 40vw
+  justify-self: center
+  border: 1px solid #D9D9D9
+  border-radius: 14px
+  box-shadow: none
+.login-input
+  width: 100%
+  border-radius: 14px
+  border: 1px solid #6C7275
+.newCommunityTxt
+  display: flex
+  align-items: center
+  justify-content: space-between
+  text-align: center
+  width: 40vw
+  justify-self: center
+.createAccBtn
+  width: 40vw
+  justify-self: center
+@media (max-width:900px)
+  .login-container,.newCommunityTxt,.createAccBtn
+    width: 90vw
+</style>
 
 <script setup>
 import { ref, onMounted } from "vue";
--- src/pages/registerPage.vue
+++ src/pages/registerPage.vue
@@ -1,26 +1,147 @@
 <template>
   <q-page>
-    <div class="flex flex-center">
-      <q-form @submit.prevent="registerStudent">
-        <q-input type="text" label="username" v-model="username" />
-        <q-input type="password" label="password" v-model="password" />
-        <q-input type="email" label="email" v-model="email" />
-        <q-input type="text" label="First Name" v-model="firstName" />
-        <q-input type="text" label="Middle Name" v-model="middleName" />
-        <q-input type="text" label="Last Name" v-model="lastName" />
-        <!-- <q-file v-model="image" label="Profile" /> -->
-        <q-btn type="submit" :loading="loading" label="submit" />
-      </q-form>
+    <div class="main-container q-pb-lg">
+      <q-card-section>
+        <div class="q-py-none">
+          <q-img
+            src="https://res.cloudinary.com/dqaw6ndtn/image/upload/v1734702951/assets/fjqfqywlp4zvtxv5rkau.png"
+            style="width: 90px; justify-self: center; display: flex"
+          />
+        </div>
+      </q-card-section>
+      <!-- Registration  form -->
+      <q-card class="registration-container q-pa-xl">
+        <q-form @submit.prevent="registerStudent">
+          <div>
+            <q-btn icon="chevron_left" to="/"></q-btn>
+          </div>
+          <q-card-section>
+            <div
+              class="text-h4"
+              style="color: #4b4b4b; font-weight: 500; text-align: center"
+            >
+              Create An Account
+            </div>
+          </q-card-section>
+
+          <q-card-section class="q-pl-none q-pb-none text-h5">
+            <div style="color: #4b4b4b">
+              <span style="color: #46af4b">*</span> Basic info
+            </div>
+          </q-card-section>
+          <!-- First Name -->
+          <q-card-section class="q-pl-none q-pb-xs">
+            <div style="color: #6c7275">First Name</div>
+          </q-card-section>
+          <div class="registration-input">
+            <q-input type="text" borderless v-model="firstName" />
+          </div>
+          <!-- Middle Name -->
+          <q-card-section class="q-pl-none q-pb-xs">
+            <div style="color: #6c7275">Middle Name</div>
+          </q-card-section>
+          <div class="registration-input">
+            <q-input type="text" borderless v-model="middleName" />
+          </div>
+          <!-- Last Name -->
+          <q-card-section class="q-pl-none q-pb-xs">
+            <div style="color: #6c7275">Last Name</div>
+          </q-card-section>
+          <div class="registration-input">
+            <q-input type="text" borderless v-model="lastName" />
+          </div>
+          <!-- prfile image -->
+          <!-- <div class="registration-input q-mt-md">
+            <q-file v-model="image" borderless label="profile" />
+          </div> -->
+          <q-card-section class="q-pl-none q-pb-none text-h5">
+            <div style="color: #4b4b4b">
+              <span style="color: #46af4b">*</span> Login info
+            </div>
+          </q-card-section>
+          <q-card-section class="q-pl-none q-pb-xs">
+            <div style="color: #6c7275">Username</div>
+          </q-card-section>
+          <!-- username -->
+          <div class="registration-input">
+            <q-input type="text" borderless v-model="username" />
+          </div>
+          <!-- Email -->
+          <q-card-section class="q-pl-none q-pb-xs">
+            <div style="color: #6c7275">Email</div>
+          </q-card-section>
+          <div class="registration-input">
+            <q-input type="email" borderless v-model="email" />
+          </div>
+          <q-card-section class="q-pl-none q-pb-xs">
+            <div style="color: #6c7275">Password</div>
+          </q-card-section>
+          <!-- Password -->
+          <div class="registration-input">
+            <q-input type="password" borderless v-model="password" />
+          </div>
+          <q-card-section class="q-pl-none q-pb-xs">
+            <div style="color: #6c7275">Confirm Password</div>
+          </q-card-section>
+          <!-- Password -->
+          <div class="registration-input">
+            <q-input type="password" borderless v-model="confirmPassword" />
+          </div>
+
+          <div style="width: 100%; border-radius: 14px" class="q-mt-md">
+            <!-- submit button -->
+            <q-btn
+              type="submit"
+              flat
+              label="Sign Up"
+              :loading="loading"
+              style="
+                width: 100%;
+                background-color: #46af4b;
+                color: #ffffff;
+                text-transform: capitalize;
+                border-radius: 14px;
+                padding: 14px 0px;
+              "
+            />
+          </div>
+        </q-form>
+      </q-card>
     </div>
   </q-page>
 </template>
 
+<style lang="sass" scoped>
+.main-container
+  height: auto
+  width: 99vw
+.registration-container
+  height: auto
+  width: 40vw
+  justify-self: center
+  border: 1px solid #D9D9D9
+  border-radius: 14px
+  box-shadow: none
+.registration-input
+  width: 100%
+  border-radius: 14px
+  border: 1px solid #6C7275
+@media (max-width: 900px)
+  .registration-container
+    width: 90vw
+</style>
+
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useQuasar } from "quasar";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
+const $q = useQuasar();
 const username = ref("");
 const password = ref("");
+const confirmPassword = ref("");
 const email = ref("");
 const firstName = ref("");
 const lastName = ref("");
@@ -29,8 +150,44 @@ const image = ref("iamge");
 const role = ref("student");
 const isArchived = ref(false);
 const loading = ref(false);
+const passwordRegex = /^(?=.*[!@#$%^&*])(?=.*\d).{8,}$/; // Regex for special char, number, and min 8 chars
 
 async function registerStudent() {
+  if (
+    !username.value ||
+    !password.value ||
+    !confirmPassword.value ||
+    !email.value ||
+    !firstName.value ||
+    !middleName.value ||
+    !lastName.value
+  ) {
+    $q.notify({
+      type: "warning",
+      message: "Please fill in all required fields.",
+    });
+    return;
+  } else if (password.value !== confirmPassword.value) {
+    $q.notify({
+      type: "warning",
+      message: "Passwords do not match! Please try again..",
+    });
+    return;
+  } else if (password.value.length < 8) {
+    $q.notify({
+      type: "warning",
+      message: "Use 8 or more characters",
+    });
+    return;
+  } else if (!passwordRegex.test(password.value)) {
+    $q.notify({
+      type: "warning",
+      message:
+        "Contain at least one special character (!@#$%^&*)\n" +
+        "Contain at least one number",
+    });
+    return;
+  }
   try {
     // const formData = new FormData();
     // formData.append("username", username.value);
@@ -60,8 +217,18 @@ async function registerStudent() {
         headers: { "Content-Type": "application/json" },
       }
     );
+    $q.notify({
+      type: "positive",
+      message: "Account Created Succesfully",
+    });
+    router.replace(`/`);
   } catch (err) {
     console.error(err);
+    $q.notify({
+      type: "negative",
+      message:
+        "An error occurred while creating the account. Please try again.",
+    });
   } finally {
     loading.value = false;
   }
